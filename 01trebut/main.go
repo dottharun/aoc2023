a9c2bd56387f550664458aea9a3a1f0c4a90f8a5
ubutharun <tharun242424@gmail.com>
2023-12-01 16:56:33 +0530

    init

---
+++ 01trebut/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getFirstNumChar(line string) (result string) {
+	for _, ch := range line {
+		if ch >= '0' && ch <= '9' {
+			result = string(ch)
+			break
+		}
+	}
+	return
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for lo, hi := 0, len(runes)-1; lo < hi; lo, hi = lo+1, hi-1 {
+		runes[lo], runes[hi] = runes[hi], runes[lo]
+	}
+	return string(runes)
+}
+
+func main() {
+	fmt.Println("Starting")
+
+	// open the doc
+	// go through each line
+	//// in each line find the end num chars and join them
+	// convert those num strings to numbers
+	//add them
+
+	// ALWAYS RUN THIS PROGRAM FROM PROJECT ROOT
+	wdir, err := os.Getwd()
+	check(err)
+
+	file, err := os.Open(path.Join(wdir, "01trebut", "data.txt"))
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	res := 0
+
+	for scanner.Scan() {
+		//getting the current line
+		line := scanner.Text()
+
+		//separating the numChars
+		numStr := getFirstNumChar(line)
+		numStr += getFirstNumChar(reverseString(line))
+
+		//string to num
+		num, err := strconv.Atoi(numStr)
+		check(err)
+		res += num
+	}
+	check(scanner.Err())
+
+	fmt.Println("ans is:", res)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dottharun/aoc2023
+
+go 1.21.3
